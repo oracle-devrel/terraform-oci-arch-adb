8782ee3387bdb2f07b0fc02df2c6716e1251f748
lfeldman <lukasz.feldman@oracle.com>
2022-04-04 12:31:53 +0200

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,11 @@
 
 output "adb_database" {
   value = {
-    adb_database_id    = oci_database_autonomous_database.adb_database.id
-    connection_urls    = oci_database_autonomous_database.adb_database.connection_urls
-    adb_wallet_content = oci_database_autonomous_database_wallet.adb_database_wallet.content
-    adb_nsg_id         = (!var.use_existing_vcn && var.adb_private_endpoint) ? oci_core_network_security_group.adb_nsg[0].id : var.adb_nsg_id
+    adb_database_id     = oci_database_autonomous_database.adb_database.id
+    connection_urls     = oci_database_autonomous_database.adb_database.connection_urls
+    adb_wallet_content  = oci_database_autonomous_database_wallet.adb_database_wallet.content
+    adb_nsg_id          = (!var.use_existing_vcn && var.adb_private_endpoint) ? oci_core_network_security_group.adb_nsg[0].id : var.adb_nsg_id
+    private_endpoint_ip = var.adb_private_endpoint ? join(",", oci_database_autonomous_database.adb_database[*].private_endpoint_ip) : ""
   }
 }
 
